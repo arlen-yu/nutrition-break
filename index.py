028652ae36f7d372f3726012ac18225be61c7e7d
Arlen Yu <arlen.yu6@gmail.com>
2016-11-06 10:46:22 -0600

    Update index.py

--- index.py
+++ index.py
@@ -41,16 +41,7 @@ def my_index_post():
 			if file and allowed_file(file.filename):
 				filename = secure_filename(file.filename)
 				file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-				nutritions = convert_to_text(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-		return '''
-		<!doctype html>
-		<title>Upload new File</title>
-		<h1>Upload new File</h1>
-		<form action="" method=post enctype=multipart/form-data>
-		  <p><input type=file name=file>
-			<input type=submit value=Upload>
-		</form>
-		'''
+				nutrients = convert_to_text(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 	return render_template("index.html", name = name, nutrients = nutrients)
 
 
